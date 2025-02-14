477f139906d9f776f7de676301a7ff7f30e0a854
Mohammed <mammegentleman@gmail.com>
2025-02-14 16:20:07 +0300

    added some changes to animations

--- script.js
+++ script.js
@@ -24,21 +24,6 @@ main.addEventListener("click", () => {
   sl.classList.remove("active");
 });
 
-//animation on scroll
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
-  });
-});
-
-const hidden = document.querySelectorAll(".hidden");
-hidden.forEach((e1) => observer.observe(e1));
-
 //alerting user that the function is unavailable
 const unavailable = document.querySelector(".notAvailable");
 
